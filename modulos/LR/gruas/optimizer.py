2bd00c29b5b6d7b86e4cb36754bf1d7163e791e8
davidcastilloalvarado <dcastilloa@uni.pe>
2021-11-13 21:20:49 -0500

    Gruas - progress - ARIMA - full - LR - full - DirRec - full

---
+++ modulos/LR/gruas/optimizer.py
@@ -0,0 +1,113 @@
+
+
+import os
+import pandas as pd
+import json
+import time
+import threading
+import optuna
+from sklearn.metrics import r2_score
+from sklearn.metrics import mean_squared_error
+from modulos.arima.gruas.general import show_results_r2, arima_forecasting, total_forecasting, show_optimizer_results
+from sklearn.linear_model import LinearRegression
+from modulos.LR.gruas.generals import make_lags
+
+
+def total_forecasting_LR(ts, n_lags):
+    X = make_lags(ts.copy(), n_lags)
+    y = pd.DataFrame({
+        'y': ts,
+    })
+    y, X = y.align(X, join='inner', axis=0)
+    model = LinearRegression()
+    model.fit(X, y)
+    y_fit = pd.DataFrame(model.predict(X), index=X.index, columns=y.columns)
+    return y_fit
+
+
+def LR_forecasting(ts, n_lags):
+    X = make_lags(ts.copy(), n_lags)
+    y = pd.DataFrame({
+        'y': ts,
+    })
+    y, X = y.align(X, join='inner', axis=0)
+    model = LinearRegression()
+    model.fit(X, y)
+    return model, X, y
+
+
+# Operaciones en multi hilo
+optuna.logging.disable_default_handler()
+
+
+class LROptimizer:
+    def __init__(self, df_time, iterations, data_path, model='LR', ):
+        self.df_time = df_time
+        self.results = pd.DataFrame()
+        self.iterations = iterations
+        self.SEED = 5050
+        self.idArticulos = df_time.columns.tolist()
+        self.model_name = model
+        self.DATA_PATH = data_path
+        self.studies = []
+
+        # self.lock = threading.Lock()
+
+    def run(self, chunk_size=4):
+
+        for i in range(0, len(self.idArticulos), chunk_size):
+            event_idarticulos = self.idArticulos[i:i + chunk_size]
+            self.worker(event_idarticulos)
+
+        self.results.to_csv(os.path.join(
+            self.DATA_PATH, 'result', self.model_name, f'{self.model_name}.csv'), index=False)
+
+    def worker(self, event_idarticulos):
+        the_threads = []
+        for idArticulo in event_idarticulos:
+            x = threading.Thread(
+                target=self.optuna_optimizer, args=(idArticulo,))
+            the_threads.append(x)
+            x.start()
+        for thread_ in the_threads:
+            thread_.join()
+
+    def optuna_optimizer(self, idArticulo):
+        def objective(trial):
+            r_min = 1
+            r_max = 6
+            n_lags = trial.suggest_int('n_lags', r_min, r_max)
+            pred = total_forecasting_LR(self.df_time[idArticulo], n_lags)
+            score = r2_score(
+                self.df_time[[idArticulo]], pred.apply(lambda x: round(x, 0)))
+            # mse = mean_squared_error(
+            #     self.df_time[[idArticulo]], pred.apply(lambda x: round(x, 0)))
+            return score
+
+        study = optuna.create_study(
+            direction='maximize', sampler=optuna.samplers.TPESampler(seed=self.SEED))
+        study.optimize(objective, n_trials=self.iterations)
+        self.save_results(idArticulo, study)
+        self.studies.append({'study': study, 'idArticulo': idArticulo})
+
+    def save_results(self, idArticulo, study):
+        row = {'idArticulo': idArticulo, 'hyper': study.best_params,
+               'r2': study.best_value, 'model': self.model_name}
+        self.results = self.results.append(row, ignore_index=True)
+
+    def print_results(self):
+        opts = pd.read_csv(os.path.join(self.DATA_PATH, 'result',
+                           self.model_name, f'{self.model_name}.csv'))
+        for opt in opts.to_dict(orient='records'):
+            hyper = json.loads(opt['hyper'].replace("'", '"'))
+            model, X, y = LR_forecasting(
+                self.df_time[opt['idArticulo']],  **hyper)
+            y_fit = pd.DataFrame(model.predict(
+                X), index=X.index, columns=y.columns)
+            r2 = show_results_r2(self.df_time, y_fit. apply(
+                lambda x: round(x, 0)), opt['idArticulo'])
+
+    def print_optimizer_results(self):
+        for study in self.studies:
+            data = [trial.value for trial in study['study'].trials]
+            show_optimizer_results(data, study['idArticulo'])
---
+++ modulos/arima/gruas/general.py
@@ -0,0 +1,91 @@
+from sklearn.impute import SimpleImputer
+import pandas as pd
+import numpy as np
+from sklearn.metrics import r2_score
+from matplotlib import pyplot as plt
+from statsmodels.tsa.arima.model import ARIMA
+
+
+def hello():
+    print("Hello from optimizer.py")
+
+
+def format_timeseries(data, index_col='Periodo', columns_id='idArticulo', value_col='ventas', fillval=0):
+    """
+    Pivot the table, fill missing values with fillval and return a dataframe
+    with the a time serie in every column.
+    """
+    imp_mean = SimpleImputer(missing_values=np.nan,
+                             strategy='constant', fill_value=fillval)
+    df_time_pre = data.pivot_table(
+        index=index_col, columns=columns_id, values=value_col, aggfunc='sum', )
+    df_time = imp_mean.fit_transform(df_time_pre)
+    df_time = pd.DataFrame(
+        df_time, columns=df_time_pre.columns, index=df_time_pre.index)
+    return df_time
+
+
+def show_results_r2(data, forecasts, idArticulo):
+    """
+    Show a graph with the results of the forecasts.
+    also with the r2 score.
+    return r2 score
+    """
+    r2 = r2_score(data[idArticulo], forecasts)
+    plt.figure(1, figsize=(12, 5))
+    plt.rcParams.update({'font.size': 14})
+    _ = plt.plot(data[[idArticulo]], color='blue',
+                 label="real",
+                 linewidth=1)
+    _ = plt.plot(forecasts, color='red',
+                 label="Forecast",
+                 linewidth=3)
+    plt.legend(loc='upper left')
+    plt.title(str(idArticulo).upper() + f" r2={round(r2,3)}")
+    plt.ylabel("Ventas UND")
+    plt.xlabel("Meses")
+    plt.grid(True)
+    plt.show(block=False)
+    return r2
+
+
+def show_optimizer_results(data, idArticulo='Place your idArticulo', color='m'):
+    """
+    Show a graph with the results of the forecasts.
+    also with the r2 score.
+    return r2 score
+    """
+    plt.figure(1, figsize=(12, 8))
+    plt.rcParams.update({'font.size': 12})
+    _ = plt.plot(data, 'o', color=color,
+                 label="real",
+                 linewidth=.5)
+    plt.title(str(idArticulo).upper() + " - optimization")
+    plt.ylabel("r2 score")
+    plt.xlabel("# Iteration")
+    plt.grid(True)
+    plt.show(block=False)
+
+
+def arima_forecasting(data, ar=2, ii=1, ma=2):
+    """
+    Forecasting using ARIMA model, 
+    return model.fit()
+    """
+    df = data.copy()
+    df.index = pd.DatetimeIndex(df.index).to_period('M')
+    model = ARIMA(df, order=(ar, ii, ma))
+    results = model.fit()
+    return results
+
+
+def total_forecasting(data, ar=2, ii=1, ma=2):
+    """
+    Forecasting using ARIMA model.
+    Return results.fittedvalues → array of fitted values
+    """
+    df = data.copy()
+    df.index = pd.DatetimeIndex(df.index).to_period('M')
+    model = ARIMA(df, order=(ar, ii, ma))
+    results = model.fit()
+    return results.fittedvalues
